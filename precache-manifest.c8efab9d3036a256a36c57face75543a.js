6823b3c7be7d4d54c4412e0c568a1ac1c92de2e9
NathanHodgkiss447 <nathanhodgkiss447@gmail.com>
2020-09-25 16:28:54 +0100

    Updates

---
+++ precache-manifest.c8efab9d3036a256a36c57face75543a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deaf5500e9fe440fcd41551eedd3b1fb",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "af84bc03c1a64d3f41ce",
+    "url": "/todoapp/static/css/main.1e3ddf79.chunk.css"
+  },
+  {
+    "revision": "b7b3c787e0542a80a5f8",
+    "url": "/todoapp/static/js/2.ab8f88eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.ab8f88eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af84bc03c1a64d3f41ce",
+    "url": "/todoapp/static/js/main.f876e5d4.chunk.js"
+  },
+  {
+    "revision": "566fce36fccc7e3661a2",
+    "url": "/todoapp/static/js/runtime-main.9b29aac0.js"
+  }
+]);
